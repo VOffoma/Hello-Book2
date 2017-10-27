42b1589cc4fc8ed1c5fb2d2ecbbc18f07dbca066
Victoria Offoma <offoma.victoria@gmail.com>
2017-10-27 12:01:32 +0100

    Setup test environment

---
+++ server/tests/index.test.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('sample test', () => {
+  it('addition of 2 and 3 should be 5', () => {
+    expect(2 + 3).to.equal(5);
+  });
+});
