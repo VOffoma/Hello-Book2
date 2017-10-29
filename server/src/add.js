8587f86a81fd5fc257326f40bcfeff304eac367b
Victoria Offoma <offoma.victoria@gmail.com>
2017-10-29 14:12:10 +0100

    Add sample functions with test

---
+++ server/src/add.js
@@ -0,0 +1,11 @@
+export function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+export function substract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+export function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
