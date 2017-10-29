b289c0b0c385afdadacd7bac7dd08667609e68eb
Offoma Victoria <VOffoma@users.noreply.github.com>
2017-10-29 14:25:15 +0100

    Integrate code coverage service (#10)
    * Add sample functions with test
    
    * Add coveralls
    
    * Add newline to .gitignore

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,7 +1,8 @@
 import { expect } from 'chai';
+import { add } from '../src/add';
 
 describe('sample test', () => {
   it('addition of 2 and 3 should be 5', () => {
-    expect(2 + 3).to.equal(5);
+    expect(add(2, 3)).to.equal(5);
   });
 });
