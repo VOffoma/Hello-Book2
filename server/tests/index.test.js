665615578bc305aaf8ac359204dddf46dfefbe4a
Victoria Offoma <offoma.victoria@gmail.com>
2017-12-01 18:18:05 +0100

    Create tests for signup and signin functionalities

--- server/tests/index.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import { add } from '../src/add';
-
-describe('sample test', () => {
-  it('addition of 2 and 3 should be 5', () => {
-    expect(add(2, 3)).to.equal(5);
-  });
-});
